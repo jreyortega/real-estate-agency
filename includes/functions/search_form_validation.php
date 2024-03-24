557b4badb91caad2553b4b7dd5b075dc4ce14bf6
anieva03 <anival03@gmail.com>
2024-03-24 11:13:39 +0100

    .

--- includes/functions/search_form_validation.php
+++ includes/functions/search_form_validation.php
@@ -17,27 +17,70 @@
 </head>
 <body>
     <?php
-    // Check if the form was submitted and the request method is POST
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $price_max=$_POST['price-max'];
-        $price_min=$_POST['price-min'];
-        $beds_max=$_POST['beds-max'];
-        $beds_min=$_POST['beds-min'];
-        $baths_max=$_POST['baths-max'];
-        $baths_min=$_POST['baths-min'];
-        $house_type=$_POST['house_type'];
-        $flat_type=$_POST['flat_type'];
-        $townhouse_type=$_POST['townhouse_type'];
-        $land_type=$_POST['land_type'];
-        $square_feet_max=$_POST['square-feet-max'];
-        $square_feet_min=$_POST['square-feet-min'];
-        $lot_size_max=$_POST['lot-size-max'];
-        $lot_size_min=$_POST['lot-size-min'];
-        $year_built_max=$_POST['year-built-max'];
-        $year_built_min=$_POST['year-built-min'];
-    } else {
-        // Redirect to the search page
-        header("Location: search.php");
-    }
+        // Check if the form was submitted and the request method is POST
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $price_max=$_POST['price-max'];
+            $price_min=$_POST['price-min'];
+            $beds_max=$_POST['beds-max'];
+            $beds_min=$_POST['beds-min'];
+            $baths_max=$_POST['baths-max'];
+            $baths_min=$_POST['baths-min'];
+            $house_type=$_POST['house_type'];
+            $flat_type=$_POST['flat_type'];
+            $townhouse_type=$_POST['townhouse_type'];
+            $land_type=$_POST['land_type'];
+            $square_feet_max=$_POST['square-feet-max'];
+            $square_feet_min=$_POST['square-feet-min'];
+            $lot_size_max=$_POST['lot-size-max'];
+            $lot_size_min=$_POST['lot-size-min'];
+            $year_built_max=$_POST['year-built-max'];
+            $year_built_min=$_POST['year-built-min'];
+        } else {
+            // Redirect to the search page
+            header("Location: search.php");
+        }
+
+        // Get the properties from the database
+        try {
+            require_once('includes/functions/db_connection.php');
+            $sql = "SELECT *
+            FROM Property
+            ORDER BY ID";
+            $results = $conn->query($sql);
+        } catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+
+        // Check if the property meets the search criteria
+        while($row = $results->fetch_assoc()) {
+            if($price_max > $row['Price'] && $price_min < $row['Price'] 
+                && $beds_max > $row['Beds'] && $beds_min < $row['Beds']
+                && $baths_max > $row['Baths'] && $baths_min < $row['Baths']
+                && $square_feet_max > $row['Sqrfeet'] && $square_feet_min < $row['Sqrfeet']
+                && $lot_size_max > $row['Lotsize'] && $$lot_size_min < $row['Lotsize']
+                && $year_built_max > $row['Year'] && $year_built_min < $row['Year']) 
+            {
+                
+                    $Prop[]=$row;
+
+                    if( $row['Type'] == $house_type )
+                    {
+                        $Prop[]=$row;
+                    }
+                    else if ( $row['Type'] == $flat_type )
+                    {
+                        $Prop[]=$row;
+                    }
+                    else if ( $row['Type'] == $townhouse_type )
+                    {
+                        $Prop[]=$row;
+                    }
+                    else if ( $row['Type'] == $land_type )
+                    {
+                        $Prop[]=$row;
+                    }
+            }
+        }
+        $conn->close();
     ?>
 </body>
--- search.php
+++ search.php
@@ -18,7 +18,7 @@
 <body>
     <div class="search-mid-body">
         <div class="search-filters">
-            <form class="search-filters-form" action=".search.php" method="post">
+            <form class="search-filters-form" action="./search.php" method="post">
                 <div class="form-price">
                     <label>Price:</label>
                     <input type="number" id="price-min" name="price-min" placeholder="max" value="0">
@@ -37,36 +37,37 @@
                 <div class="form-property-type">
                     <label>Property Type:</label>
                     <div class="house-type">
-                        <input class="checkbox-house" id="house-type" name="house-type" type="checkbox">House
+                        <input class="checkbox-house" id="house-type" name="house-type" type="checkbox" checked>House
                     </div>
                     <div class="flat-type">
-                        <input class="checkbox-house" id="flat-type" name="flat-type" type="checkbox">Flat
+                        <input class="checkbox-house" id="flat-type" name="flat-type" type="checkbox" checked>Flat
                     </div>
                     <div class="townhouse-type">
-                        <input class="checkbox-house" id="townhouse-type" name="townhouse-type" type="checkbox">Townhouse
+                        <input class="checkbox-house" id="townhouse-type" name="townhouse-type" type="checkbox" checked>Townhouse
                     </div>
                     <div class="land-type">
-                        <input class="checkbox-house" id="land-type" name="land-type" type="checkbox">Land
+                        <input class="checkbox-house" id="land-type" name="land-type" type="checkbox" checked>Land
                     </div>
                     <div class="form-property-facts">
                         <p>Property Facts:</p>
                         <div class="square-feet">
-                            <label>Sqr Feet</label>
-                            <input type="numbers" id="square-feet-max" name="square-feet-max" placeholder="max" value="99999999">
+                            <label>Sqr Feet</label>                       
                             <input type="numbers" id="square-feet-min" name="square-feet-min" placeholder="min" value="0">
+                            <input type="numbers" id="square-feet-max" name="square-feet-max" placeholder="max" value="99999999">
                         </div>
                         <div class="lot-size">
-                            <label>Lot size</label>
-                            <input type="numbers" id="lot-size-max" name="lot-size-max" placeholder="max" value="99999999" >
+                            <label>Lot size</label>                           
                             <input type="numbers" id="lot-size-min" name="lot-size-min" placeholder="min" value="0">
+                            <input type="numbers" id="lot-size-max" name="lot-size-max" placeholder="max" value="99999999" >
                         </div>
                         <div class="year-built">
-                            <label>year built</label>
-                            <input type="numbers" id="year-built-max" name="year-built-max" placeholder="max" value="99999999">
+                            <label>year built</label>     
                             <input type="numbers" id="year-built-min" name="year-built-min" placeholder="min" value="0">
+                            <input type="numbers" id="year-built-max" name="year-built-max" placeholder="max" value="99999999">
                         </div>     
                     </div>
                 </div>
+                <button type="subit"> Search </button>
             </form>
         </div>
         <div class="search-map">
@@ -74,44 +75,30 @@
         </div>
     </div>
     <div class="search-property-list">
-        <?php include_once 'includes/functions/search_form_validation.php'?>
-        <?php
-            // Get the properties from the database
-            try {
-                require_once('includes/functions/db_connection.php');
-                $sql = "SELECT *
-                FROM Property
-                ORDER BY id";
-                $results = $conn->query($sql);
-            } catch(\Exception $e) {
-                echo $e->getMessage();
-            }
+        <?php include_once 'includes/functions/search_form_validation.php'?> 
+        <script>
+            var arrayProp = <?php echo json_encode($Prop) ?> ;
 
-            // Check if the property meets the search criteria
-            while($row = $results->fetch_assoc()) {
-                if($price_max > $row['Price'] && $price_min < $row['Price'] 
-                    && $beds_max > $row['Beds'] && $beds_min < $row['Beds']
-                    && $baths_max > $row['Baths'] && $baths_min < $row['Baths']
-                    && $square_feet_max > $row['Sqrfeet'] && $square_feet_min < $row['Sqrfeet']
-                    && $lot_size_max > $row['Lotsize'] && $$lot_size_min < $row['Lotsize']
-                    && $year_built_max > $row['Year'] && $year_built_min < $row['Year']) {
-                    
-                    // Show the property
-                    echo "<div class='property'>
-                        <img src='img/property.jpg'>
-                        <div class='property-info'>
-                            <h2>" . $row['Title'] . "</h2>
-                            <p>" . $row['Description'] . "</p>
-                            <p>Price: $" . $row['Price'] . "</p>
-                            <p>Beds: " . $row['Beds'] . "</p>
-                            <p>Baths: " . $row['Baths'] . "</p>
-                            <p>Square Feet: " . $row['Sqrfeet'] . "</p>
-                            <p>Lot Size: " . $row['Lotsize'] . "</p>
-                            <p>Year Built: " . $row['Year'] . "</p>
-                        </div>";
-                }
-            }
-        ?>
+                const initApp = () =>{
+
+                    var list = document.querySelector(".search-property-list");
+
+                    arrayProp.forEach(value => {
+
+                        let newDiv = document.createElement("div");
+                        newDiv.classList.add("item");
+                        newDiv.innerHTML = `
+                        
+                            <img src = "${value.Img}/1.jpg"/>
+
+
+                        `;
+                        list.appendChild(newDiv);
+                    })
+                }  
+
+                initApp();
+        </script>
     </div>
     <?php include_once 'includes/templates/footer.php'?>
 </body>
